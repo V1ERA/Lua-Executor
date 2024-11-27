bf32d5b642345921af2cb82ecff752f573145888
Viera <v1era@proton.me>
2024-11-27 16:10:28 +0330

    first commit

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'V1ERA'
+description 'Lua executor with custom NUI'
+version '1.0.0'
+
+client_script  'client.lua'
+
+ui_page 'nui/index.html'
+
+files {
+    'nui/index.html'
+}
